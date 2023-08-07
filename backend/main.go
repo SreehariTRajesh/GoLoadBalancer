52c9e206688b94d6022713be29040ee48a77d86e
sreeharitrajesh <Sreehari T Rajesh>
2023-08-07 14:20:06 +0530

    implemented the proxy server handling

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"go_load_balancer/load_balancer"
-	"go_load_balancer/roundrobin"
+	"go_load_balancer/proxy"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
+type LoadBalancerConfig struct {
+	BalancingAlgorithm string `json:"BalancingAlgorithm"`
+}
+
 func main() {
-	roundrobin.InitializeWeightedRobin()
-	roundrobin.InitializeRoundRobin()
-	for i := 0; i < 100; i++ {
-		url := load_balancer.WeightedRoundRobinLoadBalancer()
-		fmt.Println(url)
+
+	filePath, err := filepath.Abs("configurations/LoadBalancerConfig.json")
+	if err != nil {
+		panic(err)
 	}
+	lbConfigFile, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	byteStream, _ := io.ReadAll(lbConfigFile)
+	var lbConfig LoadBalancerConfig
+	json.Unmarshal(byteStream, &lbConfig)
+	var lbFunction func() string
+	if lbConfig.BalancingAlgorithm == "RoundRobin" {
+		lbFunction = load_balancer.RoundRobinLoadBalancer
+	} else if lbConfig.BalancingAlgorithm == "WeightedRoundRobin" {
+		lbFunction = load_balancer.WeightedRoundRobinLoadBalancer
+	} else if lbConfig.BalancingAlgorithm == "HostHealthCheckCPU" {
+		lbFunction = load_balancer.HostHealthCPULoadBalancer
+	} else if lbConfig.BalancingAlgorithm == "HostHealthCheckMemory" {
+		lbFunction = load_balancer.HostHealthMemoryLoadBalancer
+	}
+	http.HandleFunc("/", proxy.ProxyHandler(lbFunction))
+	log.Println("Server Running on http://localhost:8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
